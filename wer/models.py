392e84389728ce5bbaa7bd5b7b4fc81eea1a0068
stass21 <stass21@rambler.ru>
2017-02-17 03:16:40 +0300

    3

--- wer/models.py
+++ wer/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Post(models.Model):
     name = models.CharField(max_length=20)
     res = models.IntegerField(max_length=10)
+    names = models.CharField(max_length=20)
